d0e710ca60548b42995fd3edfc23407fd795d0ce
Mayufisha <134208722+Mayufisha@users.noreply.github.com>
2024-12-24 16:17:20 -0800

    Update Specials.js

--- src/pages/Sections/Specials/Specials.js
+++ src/pages/Sections/Specials/Specials.js
@@ -44,7 +44,7 @@ const Specials = () => {
   return (
     <div className="specials">
       <div className="header-container">
-        <h1 className="specials-title">Specials</h1>
+        <h1 className="specials-title">This weaks Specials</h1>
         <Button title={"Online Menu"} onClick={onClickMenu} />
       </div>
 
