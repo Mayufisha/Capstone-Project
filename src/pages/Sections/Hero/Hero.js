9f26852c45bb7912577eb40680027bd66d512c6c
Mayufisha <134208722+Mayufisha@users.noreply.github.com>
2024-12-24 16:12:12 -0800

    Update Hero.js

--- src/pages/Sections/Hero/Hero.js
+++ src/pages/Sections/Hero/Hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import Button from "../../../components/Button/Button";
-import Image from "../../../assets/restaurantchef.jpg";
+import Image from "../../../assets/restauranfood.jpg";
 import { useNavigate } from "react-router-dom";
 import pages from "../../../utils/pages";
 
@@ -18,21 +18,19 @@ const Hero = () => {
           <h1 className="title">Little Lemon</h1>
           <h2 className="subtitle">Chicago</h2>
           <p>
-            Nestled in the heart of bustling Chicago, Little Lemon is where
-            modern flair meets cozy nostalgia. Our diverse, artisanal menu,
-            featuring delectable bruschettas, succulent burgers, and refreshing
-            Greek salads, is a testament to our belief that food is an art.
+          At Little Lemon, we bring together the rich flavors of the Mediterranean with a fresh, modern twist. Founded by brothers Mario and Adrian, who moved from Italy to pursue their dream, our restaurant blends traditional family recipes with creative new dishes inspired by Italian, Greek, and Turkish cuisine.
+Mario’s culinary skills, honed in Italy, and Adrian’s vision for expanding the menu make each dish unique, from classic bruschetta to bold, modern Mediterranean bowls. In our warm, rustic space, we invite you to relax, enjoy, and feel like part of the family.
           </p>
           <Button title={"Reserve a table"} onClick={onClickReserve} />
         </div>
 
         <img
-          className="image"
-          src={Image}
-          alt="Restarurant food"
-          height={200}
-          width={200}
-        />
+        className="image"
+        src={Image}
+        alt="Restarurant food"
+        height={200}
+        width={200}
+      />
       </div>
     </div>
   );
